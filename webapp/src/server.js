e6dbc50c0e915dcffa69e2400ee8fd935da3f038
Youri K <youri.kaminsky@student.hpi.de>
2017-11-09 15:59:09 +0100

    added mysql connection and setup function #1

--- webapp/src/server.js
+++ webapp/src/server.js
@@ -1,17 +1,67 @@
-'use strict';
+"use strict";
 
-const express = require('express');
+const express = require("express");
+var mysql = require("mysql");
+
+var con = mysql.createConnection({
+  host: "db",
+  user: "root",
+  password: "myTeletunesPw",
+  database: "teletunes"
+});
 
 // Constants
 const PORT = 8080;
-const HOST = '0.0.0.0';
+const HOST = "0.0.0.0";
 
 // App
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello TeleTask\n');
+app.get("/", (req, res) => {
+  res.send("Hello TeleTask\n");
 });
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
- 
+setup();
+
+function setup() {
+  var connection = mysql.createConnection({
+    host: "db",
+    user: "root",
+    password: "myTeletunesPw"
+  });
+  connection.connect(function(err) {
+    if (err) throw err;    
+
+    connection.query("SHOW DATABASES", function(err, result) {
+      if (err) throw err;
+      if (
+        result.some(val => {
+          return val.Database === "teletunes";
+        })
+      ) {
+        con.connect(function(err) {
+          if (err) throw err;
+          console.log("Connected!");
+        });
+      } else {
+        console.log("else");
+
+        connection.query("CREATE DATABASE teletunes", function(err, result) {
+          if (err) throw err;
+          console.log("Database created");
+          con.connect(function(err) {
+            if (err) throw err;
+            con.query(
+              "CREATE TABLE data (id INT PRIMARY KEY AUTO_INCREMENT, date VARCHAR(100), itunes_id VARCHAR(100), content_title VARCHAR(100), browse INT, subscribe INT, download INT, stream INT, auto_download INT)",
+              function(err, result) {
+                if (err) throw err;
+                console.log("Table created");
+              }
+            );
+          });
+        });
+      }
+    });
+  });
+}
