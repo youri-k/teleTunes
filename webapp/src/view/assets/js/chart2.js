209f52c24e41007b7c53d3f8a502e8ab8b176270
Youri K <youri.kaminsky@student.hpi.de>
2018-02-02 13:29:35 +0100

    added dynamic scaling to charts

--- webapp/src/view/assets/js/chart2.js
+++ webapp/src/view/assets/js/chart2.js
@@ -1,85 +1,97 @@
+function showChart2(onACanvas, withTitle, start = null, end = null) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      dataToChart2(JSON.parse(this.responseText));
+    }
+  };
 
-function showChart2(onACanvas, withTitle, start =null , end = null){
-        var xhttp = new XMLHttpRequest();
-                xhttp.onreadystatechange = function() {
-                    if (this.readyState == 4 && this.status == 200) {
-                        dataToChart2(JSON.parse(this.responseText));
-                    }
-                };
-                
-                var url = "http://localhost:8080/maxInteractionsInInterval?fields=download&limit=10";
-                if(start != null)
-                    url += "&startDate=" + start;
-                if(end != null)
-                    url += "&endDate=" + end;
-                xhttp.open("GET", url, true);
-                xhttp.send();
-                
-                function dataToChart2(responseData) {
-                    var labels = [];
-                    var data = [];
-                    responseData.forEach(item => {
-                        labels.push(item.title); data.push(item.sum);
-                    });
-                var ctx = document.getElementById(onACanvas).getContext('2d');
-                var myChart2 = new Chart(ctx, {
-                    type: 'bar',
-                    data: {
-                        labels: labels,
-                        datasets: [{
-                            data: data,
-                        }]
-                    },
-                    options: {
-                        legend: {
-                            display: false,
-                        },
-                        title: {
-                            display: true,
-                            text: withTitle,
-                            fontSize: 32
-                        },
-                        scales: {
-                            yAxes: [{
-                                ticks: {
-                                    stepSize: 200
-                                }, 
-                                scaleLabel: {
-                                            display: true,
-                                            labelString: 'Anzahl Gesamtdownloads',
-                                            fontSize: 24
-                                        }
-                                    }],
-                                    xAxes: [{
-                                        ticks: {
-                                            autoSkip: false
-                                        },
-                                        scaleLabel: {
-                                            display: true,
-                                            labelString: 'Namen der Kurse',
-                                            fontSize: 24
-                                      }
-                                    }]
-                                },
-                        tooltips: {
-                            callbacks: {
-                                title: function(tooltipItem, data) {
-                                    return data['labels'][tooltipItem[0]['index']];
-                                },
-                                label: function(tooltipItem, data) {
-                                    var respObj = responseData[tooltipItem['index']];
-                                    var labelArr = [];
-                                    labelArr.push("Downloads: " + respObj.download);
-                                    labelArr.push("Browse: " + respObj.browse);
-                                    labelArr.push("Subscriptions: " + respObj.subscribe);
-                                    labelArr.push("Streams: " + respObj.stream);
-                                    labelArr.push("AutoDownloads: " + respObj.auto_download);
-                                    return labelArr;
-                                },
-                            },
-                            displayColors: false
-                        }
-                    }
-                });
-            } 
+  var url =
+    "http://localhost:8080/maxInteractionsInInterval?fields=download&limit=10";
+  if (start != null) url += "&startDate=" + start;
+  if (end != null) url += "&endDate=" + end;
+  xhttp.open("GET", url, true);
+  xhttp.send();
+
+  function dataToChart2(responseData) {
+    var labels = [];
+    var data = [];
+    var maximum = 0;
+    responseData.forEach(item => {
+      labels.push(item.title);
+      data.push(item.sum);
+      if (item.sum > maximum) maximum = item.sum;
+    });
+    var maxYAxe = Math.ceil(maximum / 100) * 100;
+    var tickRate = Math.ceil(maxYAxe / 4000) * 200;
+    maxYAxe = Math.ceil(maximum / tickRate) * tickRate;
+    var ctx = document.getElementById(onACanvas).getContext("2d");
+    var myChart2 = new Chart(ctx, {
+      type: "bar",
+      data: {
+        labels: labels,
+        datasets: [
+          {
+            data: data
+          }
+        ]
+      },
+      options: {
+        legend: {
+          display: false
+        },
+        title: {
+          display: true,
+          text: withTitle,
+          fontSize: 32
+        },
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                min: 0,
+                max: maxYAxe,
+                stepSize: tickRate
+              },
+              scaleLabel: {
+                display: true,
+                labelString: "Anzahl Gesamtdownloads",
+                fontSize: 24
+              }
+            }
+          ],
+          xAxes: [
+            {
+              ticks: {
+                autoSkip: false
+              },
+              scaleLabel: {
+                display: true,
+                labelString: "Namen der Kurse",
+                fontSize: 24
+              }
+            }
+          ]
+        },
+        tooltips: {
+          callbacks: {
+            title: function(tooltipItem, data) {
+              return data["labels"][tooltipItem[0]["index"]];
+            },
+            label: function(tooltipItem, data) {
+              var respObj = responseData[tooltipItem["index"]];
+              var labelArr = [];
+              labelArr.push("Downloads: " + respObj.download);
+              labelArr.push("Browse: " + respObj.browse);
+              labelArr.push("Subscriptions: " + respObj.subscribe);
+              labelArr.push("Streams: " + respObj.stream);
+              labelArr.push("AutoDownloads: " + respObj.auto_download);
+              return labelArr;
+            }
+          },
+          displayColors: false
+        }
+      }
+    });
+  }
 }
--- webapp/src/view/assets/js/chart3.js
+++ webapp/src/view/assets/js/chart3.js
@@ -1,43 +1,94 @@
-function showChart3(onACanvas){
-    var ctx = document.getElementById(onACanvas).getContext('2d');
-                var myChart3 = new Chart(ctx, {
-                    type: 'line',
-                    data: {
-                        labels: ["Datum_1", "Datum_2", "Datum_3", "Datum_4", "Datum_5", "Datum_6"],
-                        datasets: [{
-                            data: [12, 19, 3, 5, 2, 3]
-                        }]
-                    },
-                    options: {
-                        legend: {
-                            display: false,
-                        },
-                        title: {
-                            display: true,
-                            text: 'Anzahl Downloads | Zeitraum',
-                            fontSize: 32
-                        },
-                        scales: {
-                            yAxes: [{
-                                ticks: {
-                                    min: 0,
-                                    max: 100,
-                                    stepSize: 25
-                                },
-                                scaleLabel: {
-                                    display: true,
-                                    labelString: 'Anzahl Gesamtdownloads | Kurs',
-                                    fontSize: 24
-                                }
-                            }],
-                            xAxes: [{
-                                scaleLabel: {
-                                    display: true,
-                                    labelString: 'Zeitraum',
-                                    fontSize: 24
-                              }
-                            }]
-                        }
-                    }
-                });
+function showChart3(onACanvas) {
+  var params = ["download", "subscribe"];
+
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      dataToChart(JSON.parse(this.responseText));
+    }
+  };
+  xhttp.open(
+    "GET",
+    "http://localhost:8080/api/course?fields=" +
+      params.join(",") +
+      "&id=728270905",
+    true
+  );
+  xhttp.send();
+
+  function dataToChart(responseData) {
+    var maximum = 0;
+    var labels = [];
+    var datasets = [];
+    params.forEach(param => {
+      var dataObj = {};
+      var paramArray = [];
+      dataObj.data = paramArray;
+      datasets.push(dataObj);
+    });
+    responseData.forEach(item => {
+      labels.push(item.date);
+      params.forEach((param, index) => {
+        datasets[index].data.push(item[param]);
+        if (item[param] > maximum) maximum = item[param];
+      });
+    });
+    var maxYAxe = Math.ceil(maximum / 50) * 50;
+    var tickRate = Math.ceil(maxYAxe / 1000) * 50;
+    maxYAxe = Math.ceil(maximum / tickRate) * tickRate;
+    var ctx = document.getElementById(onACanvas).getContext("2d");
+    var myChart3 = new Chart(ctx, {
+      type: "line",
+      data: {
+        labels: labels,
+        datasets: datasets
+      },
+      options: {
+        legend: {
+          display: false
+        },
+        title: {
+          display: true,
+          text: "Anzahl Downloads / Zeitraum",
+          fontSize: 32
+        },
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                min: 0,
+                max: maxYAxe,
+                stepSize: tickRate
+              },
+              scaleLabel: {
+                display: true,
+                labelString: "Anzahl Gesamtdownloads | Kurs",
+                fontSize: 24
+              }
+            }
+          ],
+          xAxes: [
+            {
+              scaleLabel: {
+                display: true,
+                labelString: "Zeitraum",
+                fontSize: 24
+              }
+            }
+          ]
+        },
+        tooltips: {
+          callbacks: {
+            title: function(tooltipItem, data) {
+              return data["labels"][tooltipItem[0]["index"]];
+            },
+            label: function(tooltipItem, data) {
+              return //data["datasets"][tooltips]; //TODO
+            }
+          },
+          displayColors: false
+        }
+      }
+    });
+  }
 }
